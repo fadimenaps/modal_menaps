9fadf8518c5f606569137350e0eed3c50aa6e50e
Fadi Mathlouthi <fadi.mathlouthi@menaps.com>
2022-05-16 09:31:23 +0100

    first commit

---
+++ src/Modal.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace LivewireUI\Modal;
+
+use Exception;
+use Livewire\Component;
+use ReflectionClass;
+
+class Modal extends Component
+{
+    const TAILWIND  = 'tailwind';
+    const BOOTSTRAP = 'bootstrap';
+    
+    public ?string $activeComponent;
+
+    public array $components = [];
+
+    public function resetState(): void
+    {
+        $this->components = [];
+        $this->activeComponent = null;
+    }
+
+    public function openModal($component, $componentAttributes = [], $modalAttributes = []): void
+    {
+        $requiredInterface = \LivewireUI\Modal\Contracts\ModalComponent::class;
+        $componentClass = app('livewire')->getClass($component);
+        $reflect = new ReflectionClass($componentClass);
+
+        if ($reflect->implementsInterface($requiredInterface) === false) {
+            throw new Exception("[{$componentClass}] does not implement [{$requiredInterface}] interface.");
+        }
+
+        $id = md5($component . serialize($componentAttributes));
+        $this->components[$id] = [
+            'name'            => $component,
+            'attributes'      => $componentAttributes,
+            'modalAttributes' => array_merge([
+                'closeOnClickAway' => $componentClass::closeModalOnClickAway(),
+                'closeOnEscape' => $componentClass::closeModalOnEscape(),
+                'closeOnEscapeIsForceful' => $componentClass::closeModalOnEscapeIsForceful(),
+                'dispatchCloseEvent' => $componentClass::dispatchCloseEvent(),
+                'destroyOnClose' => $componentClass::destroyOnClose(),
+                'maxWidth' => $componentClass::modalMaxWidth(),
+                'maxWidthClass' => $componentClass::modalMaxWidthClass(),
+            ], $modalAttributes),
+        ];
+
+        $this->activeComponent = $id;
+
+        $this->emit('activeModalComponentChanged', $id);
+    }
+
+    public function destroyComponent($id): void
+    {
+        unset($this->components[$id]);
+    }
+
+    public function getListeners(): array
+    {
+        return [
+            'openModal',
+            'destroyComponent'
+        ];
+    }
+
+    public function render()
+    {
+        return view('livewire-ui::' . modalFramework());
+    }
+}
